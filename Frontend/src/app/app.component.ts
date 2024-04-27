5a035900cc9ea63b81d9268b9fd519a5b2302335
Khushal Sherki <154081520+Khushal13-sh@users.noreply.github.com>
2024-04-27 16:42:17 +0530

    Add files via upload

---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'calculator2';
+
+  constructor(public http:HttpClient){
+  }
+
+  baseUrl="http://13.235.80.153:8080/calculatorui/";
+  num1=0;
+  num2=0;
+  result=0;
+
+  sum(){
+    let url=this.baseUrl+'add'+this.num1+'and'+this.num2;
+    this.http.get(url).subscribe((data:any)=>{this.result=data})
+  }
+
+  sub(){
+    let url=this.baseUrl+'sub'+this.num1+'and'+this.num2;
+    this.http.get(url).subscribe((data:any)=>{this.result=data})
+  }
+
+  mul(){
+    let url=this.baseUrl+'mult'+this.num1+'and'+this.num2;
+    this.http.get(url).subscribe((data:any)=>{this.result=data})
+  }
+
+  div(){
+    let url=this.baseUrl+'div'+this.num1+'and'+this.num2;
+    this.http.get(url).subscribe((data:any)=>{this.result=data})
+  }
+
+  mod(){
+    let url=this.baseUrl+'mod'+this.num1+'and'+this.num2;
+    this.http.get(url).subscribe((data:any)=>{this.result=data})
+  }
+}
